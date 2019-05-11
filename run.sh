ec435a269ac2c699a20dbae63be46cb7edc29a76
Mihai A <mihai.anei@gmail.com>
2019-05-11 22:56:45 +0300

    Some tweaks and notes for python app

--- run.sh
+++ run.sh
@@ -14,16 +14,12 @@ do
     echo "Starting up docker container for $project!"
     
     docker run --name $project -d -p 8080:8080 --memory=1g --cpus=2 $project:latest
+
+    sleep 5;
     
-    sleep 1;
+    echo "Running actual benchmark for 60s!"
     
-    echo "Running warmup for 30s!"
-    
-    wrk -t4 -c24 -d30s -s post.lua --latency http://localhost:8080/benchmark
-    
-    echo "Running actual benchmark for 10s!"
-    
-    WRK_REPORT=${project}-$(date +"%m-%d-%Y-%T").csv wrk -t4 -c24 -d10s --latency -s benchmark.lua http://localhost:8080/benchmark
+    WRK_REPORT=${project}-$(date +"%m-%d-%Y-%T").csv wrk -t4 -c24 -d60s --latency -s benchmark.lua http://localhost:8080/benchmark
     
     echo "Cleaning up!"
     docker rm $(docker stop -t 1 ${project})
